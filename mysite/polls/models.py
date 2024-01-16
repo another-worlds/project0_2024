8fe240c45def5764ea35b3d901c7b3fad4df468d
another-worlds <a.egorshev@gmail.com>
2024-01-16 20:34:37 +0600

    Milestone 3 done

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Post(models.Model):
+    
+    title = models.CharField(max_length=48)
+    text = models.TextField()
+    score = models.IntegerField(default=0)
+    pub_date = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return f"This is a post titled {self.title} from {self.pub_date} with {self.score} score"
+        
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    
+    text = models.TextField()
+    score = models.IntegerField(default=0)
+    pub_date = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return f"Comment to a post #{self.post.id} with {self.score} score"
